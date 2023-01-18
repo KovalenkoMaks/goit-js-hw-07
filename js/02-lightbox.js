7b3eb340c3f5afe9e8c2acf23836e73f88bd4555
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-18 21:30:36 +0200

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryItems
+  .map(
+    (elem) => `<a class="gallery__item" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" title="${elem.description}" />
+</a>`
+  )
+  .join("");
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+  });
+});
